1e46b7f18f5424cb5b1a7e9e626eda5c5300d6a5
Curtis Wilson <curtiswemail@gmail.com>
2018-06-15 20:05:30 -0600

    Add message functionality

--- lib/kele.rb
+++ lib/kele.rb
@@ -31,4 +31,24 @@ class Kele
     puts avail_array
   end
 
+  def get_messages(page = 0)
+    if page > 0
+      message_threads_url = "/message_threads/?page=#{page}"
+    else
+      message_threads_url = "/message_threads"
+    end
+
+    response = self.class.get(message_threads_url, headers: {"authorization" => @auth_token})
+    puts response
+  end
+
+  def create_message(sender, recipient_id, subject, stripped_text)
+        response = self.class.post("/messages", headers: {"authorization" => @auth_token}, body:{
+          "sender": sender,
+          "recipient_id": recipient_id,
+          "subject": subject,
+          "stripped-text": stripped_text
+          })
+          response.success? puts 'Message was sent!'
+  end
 end
